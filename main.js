a271e825ee00f299c2df8732167ffb5c9edbc3f4
Timothy Ross Grey <TimGrey@Timothys-MacBook-Pro-7.local>
2019-06-26 13:30:01 -0700

    Added addEventListeners to ABOUT / LOGIN / SIGN UP.  Black to gray onmouseover.

--- main.js
+++ main.js
@@ -1,18 +1,55 @@
 
-const a= document.getElementById("test").addEventListener("mouseover", mouseOver);
-    console.log(a);
-document.getElementById("test").addEventListener("mouseout", mouseOut);
+// const a= document.getElementById("test").addEventListener("mouseover", mouseOver);
+//     console.log(a);
+// document.getElementById("test").addEventListener("mouseout", mouseOut);
 
-function mouseOver() {
-    document.getElementById("test").style.color = "magenta";
-}
+// function mouseOver() {
+//     document.getElementById("test").style.color = "magenta";
+// }
 
-function mouseOut() {
-  document.getElementById("test").style.color = "black";
-}
-
-
-
-
+// function mouseOut() {
+//   document.getElementById("test").style.color = "black";
+// }
 
 // elt.setAttribute("style", "color:red; border: 1px solid blue;");
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+
+
+// nav-link
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%  ABOUT  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+let var1= document.getElementById("nav-link1").onmouseover = function() {mouseOver()};
+let var2= document.getElementById("nav-link1").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+  document.getElementById("nav-link1").style.color = "gray";
+}
+function mouseOut() {
+  document.getElementById("nav-link1").style.color = "black";
+}
+
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%  LOGIN  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+let var3= document.getElementById("nav-link2").onmouseover = function() {mouseOver2()};
+let var4= document.getElementById("nav-link2").onmouseout = function() {mouseOut2()};
+
+function mouseOver2() {
+  document.getElementById("nav-link2").style.color = "gray";
+}
+function mouseOut2() {
+  document.getElementById("nav-link2").style.color = "black";
+}
+
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%  SIGN UP  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+let var5= document.getElementById("nav-link3").onmouseover = function() {mouseOver3()};
+let var6= document.getElementById("nav-link3").onmouseout = function() {mouseOut3()};
+
+function mouseOver3() {
+  document.getElementById("nav-link3").style.color = "gray";
+}
+function mouseOut3() {
+  document.getElementById("nav-link3").style.color = "black";
+}
