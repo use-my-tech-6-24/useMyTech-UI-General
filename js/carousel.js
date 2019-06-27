bef7728b660e7430043d018c61636659f4ae5c82
pj-wise <51010291+pj-wise@users.noreply.github.com>
2019-06-27 12:35:01 -0400

    added animation to carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,7 @@
 let sliderImages = document.querySelectorAll(".slide"),
   arrowLeft = document.querySelector("#arrow-left"),
   arrowRight = document.querySelector("#arrow-right"),
+  slideContent = document.querySelectorAll('.hero-text'),
   current = 0;
 
 // Clear all images
@@ -16,11 +17,18 @@ function startSlide() {
   sliderImages[0].style.display = "block";
 }
 
+function startSlideContent(){
+  slideContent[0].classList.add('animated', 'fadeInDown', 'slow');
+}
+
 // Shows prev
 function slideLeft() {
   reset();
   sliderImages[current - 1].style.display = "block";
   current--;
+}
+function slideContentLeft(){
+    slideContent[current - 1].classList.add('animated', 'flipInX');
 }
 
 // Shows next
@@ -29,13 +37,16 @@ function slideRight() {
   sliderImages[current + 1].style.display = "block";
   current++;
 }
-
+function slideContentRight(){
+  slideContent[current + 1].classList.add('animated', 'flipInX');
+}
 // Left arrow click
 arrowLeft.addEventListener("click", function() {
   if (current === 0) {
     current = sliderImages.length;
   }
   slideLeft();
+  slideContentLeft();
 });
 
 // Right arrow click
@@ -43,7 +54,9 @@ arrowRight.addEventListener("click", function() {
   if (current === sliderImages.length - 1) {
     current = -1;
   }
+  slideContentRight();
   slideRight();
 });
 
 startSlide();
+startSlideContent();
