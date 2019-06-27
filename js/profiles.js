3b190de1e7abd0d2f35c56d5c28e305b2bc3acda
tatiana faramarzi <tfaramarzi@gmail.com>
2019-06-27 10:04:35 -0700

    updated js file name

--- js/profiles.js
+++ js/profiles.js
@@ -1,8 +1,11 @@
 
+//Assigns element to its DOM reference and adds relevant data
 class TabLink {
     constructor(tabElement){
         this.tabElement = tabElement;
         this.tabData = tabElement.dataset.tab;
+
+//Creates conditional statement for All versus other tabs        
         
         if(this.tabData === 'all'){
             this.profiles = document.querySelectorAll('.profile')
@@ -10,12 +13,18 @@ class TabLink {
             this.profiles = document.querySelectorAll(`.profile[data-tab='${this.tabElement.dataset.tab}']`);
         }
 
+//Converts each profile class element into a new TabProfile      
+
         this.profiles = Array.from(this.profiles).map(function(profile){
             return new TabProfile(profile);
         });
 
+//Adds event listener        
+
         this.tabElement.addEventListener('click', () => this.selectTab());
     };
+
+//Creates event response function    
 
     selectTab(){
         const tabs = document.querySelectorAll('.tab');
@@ -32,6 +41,8 @@ class TabLink {
     }
 }
 
+//Assigns profile element to its DOM reference and creates event response
+
 class TabProfile {
     constructor(profileElement){
         this.profileElement = profileElement; 
@@ -42,10 +53,12 @@ class TabProfile {
     }
 }
 
-//Assigns tabs to variables below
+//Assigns tabs to tabs variables
 
 const tabs = document.querySelectorAll('.tab');
 console.log(tabs);
+
+//Returns new instance of TabLink
 
 tabs.forEach(function(tab){
     return new TabLink(tab);
